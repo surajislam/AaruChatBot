5d1cba676f454d8a7d8520c66b844f7389caddeb
GOD RAVAN <167273277+GOD-R4V4N@users.noreply.github.com>
2024-04-21 08:06:06 +0530

    ping.py

--- Asuraa/modules/ping.py
+++ Asuraa/modules/ping.py
@@ -28,7 +28,7 @@ async def ping(_, message: Message):
 
     ms = (datetime.now() - start).microseconds / 1000
     await loda.edit_text(
-        text=f"**๏ ʜᴇʏ ʙᴀʙʏ..!\n\n๏ {BOT_NAME} ɪs ᴀʟɪᴠᴇ ᴀɴᴅ ᴡᴏʀᴋɪɴɢ ғɪɴᴇ.\n\n๏ ᴘɪɴɢ ᴘᴏɴɢ ➥ `{ms}` ᴍs\n\n๏ ᴍᴀᴅᴇ ʙʏ ➛ [ʀᴏʏ-ᴇᴅɪᴛx](https://t.me/{OWNER_USERNAME}) !**",
+        text=f"**๏ ʜᴇʏ ʙᴀʙʏ..!\n\n๏ {BOT_NAME} ɪs ᴀʟɪᴠᴇ ᴀɴᴅ ᴡᴏʀᴋɪɴɢ ғɪɴᴇ.\n\n๏ ᴘɪɴɢ ᴘᴏɴɢ ➥ `{ms}` ᴍs\n\n๏ ᴍᴀᴅᴇ ʙʏ ➛ [ɢᴏᴅ ʀᴀᴠᴀɴ](https://t.me/{OWNER_USERNAME}) !**",
         reply_markup=InlineKeyboardMarkup(PNG_BTN),
     )
     if message.chat.type == ChatType.PRIVATE:
